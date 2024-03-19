783e4ffa254556bc45d675499902fe50a55edbe1
karlo <karlsgit1126@gmail.com>
2024-03-19 14:33:25 +0800

    setting up font family

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    theme: {
+        extend: {
+            screens: {
+                sm: "576px",
+                md: "768px",
+                lg: "992px",
+                xl: "1200px",
+                "2xl": "1400px",
+            },
+            colors: {
+                dark: "#1f1f1f",
+                primary: "#feb900",
+                smoke: "#6c757d",
+                lightenDark: "#313131",
+            },
+        },
+    },
+    plugins: [],
+};
