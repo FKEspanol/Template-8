4e8d047d7e0c0bcf259c4089ce1d6735399adc29
karlo <karlsgit1126@gmail.com>
2024-03-23 19:47:30 +0800

    changed site title

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = "Logis";
+export const SITE_TITLE = "Template-8";
 export const SITE_DESCRIPTION =
     "A logis Bootstrap template visual replica using Astro.js and tailwindcss";
 export const AUTHOR_NAME = "Karlo Espanol";
